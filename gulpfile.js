529a62ca1ed5113c2cfe5cdee77bcd1f1c72f726
Elliott O'Hara <elliott.ohara@gmail.com>
2017-08-28 20:35:14 -0500

    Added e2e npm task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
 
+gulp.task('clean', () => {
+  gulp.src('dist', {read: false}).pipe(clean());
+});
+
 gulp.task('scripts', () => {
+  
   const tsResult = tsProject.src()
   .pipe(tsProject());
   return tsResult.js.pipe(gulp.dest('dist'));
